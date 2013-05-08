e4cb9a28055f0fde4263c31374087620e782021f
Michael Seaton <mseaton@pih.org>
2013-05-08 12:12:17 -0400

    HTMLWIDGETS-13: Error handling Widget: org.openmrs.Location

--- omod/src/main/java/org/openmrs/module/htmlwidgets/web/taglib/WidgetTag.java
+++ omod/src/main/java/org/openmrs/module/htmlwidgets/web/taglib/WidgetTag.java
@@ -126,7 +126,20 @@ public class WidgetTag extends TagSupport {
 		config.setType(fieldType);
 		config.setGenericTypes(genericFieldTypes);
 		config.setFormat(getFormat());
-		config.setDefaultValue(propertyValue != null ? propertyValue : getDefaultValue());
+
+		Object defaultValue = propertyValue;
+		if (defaultValue == null) {
+			defaultValue = getDefaultValue();
+			try {
+				if (defaultValue != null && defaultValue instanceof String) {
+					defaultValue = handler.parse((String)defaultValue, fieldType);
+				}
+			}
+			catch (Exception e) {
+				throw new IllegalArgumentException("The default value of " + defaultValue + " is not valid for handler " + handler.getClass(), e);
+			}
+		}
+		config.setDefaultValue(defaultValue);
 		
 		config.setFixedAttribute("id", getId());
 		config.setFixedAttribute("name", getName());
