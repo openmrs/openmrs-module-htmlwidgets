a3dc87aa0aa6ff409230775c7087b3dcc03f6f62
Wikum Weerakutti <wikum@linuxdeveloper.space>
2025-05-16 18:11:35 +0530

    HTMLWIDGETS-26: Migrate to Platfrom 2.7.0 and Support Java 21

---
+++ omod/src/main/java/org/openmrs/module/htmlwidgets/web/demo/PatientLocationMethod.java
@@ -0,0 +1,12 @@
+package org.openmrs.module.htmlwidgets.web.demo;
+
+public enum PatientLocationMethod {
+    EARLIEST_ENCOUNTER,
+    LATEST_ENCOUNTER,
+    ANY_ENCOUNTER,
+    PATIENT_HEALTH_CENTER;
+
+    PatientLocationMethod() {
+    }
+
+}
